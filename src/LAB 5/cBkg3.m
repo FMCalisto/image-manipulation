f0761519183a74260f6bed020bdbed622b24cf12
pmbrs <pedro.ricardo.silva@tecnico.ulisboa.pt>
2017-03-28 16:28:58 +0100

    cBkg3_v2

---
+++ src/LAB 5/cBkg3.m
@@ -0,0 +1,20 @@
+path='Ist\'; frameIdComp=4;
+str=['%s%.' num2str(frameIdComp) 'd.%s'];
+
+nFrame=548;
+step=4;
+
+vid4D= zeros([288 360 3 nFrame/step]);
+figure;
+for k=1 : 1 : nFrame/step
+    k
+    str1=sprintf(str,path,k,'jpg');
+    img=imread(str1);
+    vid4D(:,:,:,k)=img;
+    imshow(img); drawnow
+    hold off
+    %pause(0.2)
+end
+
+bkg= median(vid4D,4);
+figure; imagesc(uint8(bkg));
