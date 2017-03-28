cfba973fa73770762faa50fc75b66d71ef03d219
pmbrs <pedro.ricardo.silva@tecnico.ulisboa.pt>
2017-03-28 16:36:45 +0100

    1ex

--- src/LAB 5/cBkg.m
+++ src/LAB 5/cBkg.m
@@ -19,3 +19,29 @@ end
 bkg = median(vid4D, 4);
 figure,
 imshow(uint8(bkg));
+
+
+vid = VideoReader('Movie_0002.avi');
+nFrames = 40 * 25;
+step = 20;
+
+img=read(vid,1);
+Bkg=zeros (size(img));
+
+figure; hold on
+alpha=0.05; %Exprimentar varios valores para ALPHA
+
+for i = 1 : step : nFrames
+    i
+    img = read(vid, i);
+    vid4D(:,:,:,k) = img;
+    imshow(img); drawnow
+    k = k + 1;
+    %pause
+end
+
+bkg = median(vid4D, 4);
+figure,
+imshow(uint8(bkg));
+
+
