fbbbfb8cbe99a5896b7f5f64cecdd7ef3767cbba
Francisco Maria Calisto <email@franciscocalisto.me>
2017-03-28 16:03:01 +0100

    [CREATE] LAB 5

---
+++ src/LAB 5/cBkg.m
@@ -0,0 +1,21 @@
+vid = VideoReader('Movie_0002.avi');
+
+nFrames = 40 * 25;
+step = 20;
+
+vid4D = zeros([vid.Height vid.Width 3 nFrame/step]);
+figure,
+k = 1;
+
+for i = 1 : step : nFrame
+    i
+    img = read(vid, i);
+    vid4D(:,:,:,k) = img;
+    imshow(img); drawnow
+    k = k + 1;
+    %pause
+end
+
+bkg = median(vid4D, 4);
+figure,
+imshow(uint8(bkg));
---
+++ src/MATERIAL/Ist/param.m
@@ -0,0 +1,19 @@
+% limiar para pixel activo
+global T;
+T = 0.1;
+
+% area minima para ser considerada regiao
+global minarea;
+minarea = 50;
+
+% primeira frame
+global fframe;
+fframe = 0;
+
+% ultima frame
+global lframe;
+lframe = 549;
+
+% numero minimo de regioes emparelhadas para ser considerado segmento
+global strokeThr;
+strokeThr = 3;
